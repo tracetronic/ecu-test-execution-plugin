22725310d4f6a426c5c29e1a2eabed6450cb211a
ChristophW <christoph.wellm@tracetronic.de>
2022-04-21 15:54:08 +0200

    Add unit tests for builders (#21)

--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/builder/TestPackageBuilderTest.groovy
+++ src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/builder/TestPackageBuilderTest.groovy
@@ -1,120 +1,66 @@
 package de.tracetronic.jenkins.plugins.ecutestexecution.builder
 
-import de.tracetronic.cxs.generated.et.client.model.AdditionalSettings
-import de.tracetronic.cxs.generated.et.client.model.Execution
 import de.tracetronic.cxs.generated.et.client.model.ExecutionOrder
-import de.tracetronic.cxs.generated.et.client.model.LabeledValue
-import de.tracetronic.cxs.generated.et.client.model.Recording
-import de.tracetronic.jenkins.plugins.ecutestexecution.IntegrationTestBase
 import de.tracetronic.jenkins.plugins.ecutestexecution.configs.AnalysisConfig
 import de.tracetronic.jenkins.plugins.ecutestexecution.configs.ExecutionConfig
 import de.tracetronic.jenkins.plugins.ecutestexecution.configs.PackageConfig
 import de.tracetronic.jenkins.plugins.ecutestexecution.configs.TestConfig
-import de.tracetronic.jenkins.plugins.ecutestexecution.model.AdditionalSetting
-import de.tracetronic.jenkins.plugins.ecutestexecution.model.Constant
-import de.tracetronic.jenkins.plugins.ecutestexecution.model.PackageParameter
-import hudson.Launcher
 import org.jenkinsci.plugins.workflow.steps.StepContext
-import org.junit.Rule
-import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
 
-class TestPackageBuilderTest extends IntegrationTestBase {
-
-    @Rule
-    TemporaryFolder folder = new TemporaryFolder()
-
-    Launcher launcher
-    StepContext context
-
-    def setup() {
-        launcher = jenkins.createOnlineSlave().createLauncher(jenkins.createTaskListener())
-        context = Mock()
-        context.get(Launcher.class) >> launcher
-    }
+class TestPackageBuilderTest extends Specification {
 
     def 'Test Get Artifact Name'() {
         given:
-            TestPackageBuilder testPackageBuilder = new TestPackageBuilder(null, null, null, null, null, null)
+            TestPackageBuilder builder = new TestPackageBuilder(null, null, null, null, null, null)
 
         expect:
-            testPackageBuilder.getTestArtifactName() == 'package'
+            builder.getTestArtifactName() == 'package'
     }
 
-    def 'Test Empty Execution Order'() {
+    def 'Test Null Values'() {
         given:
-            final File testFile = folder.newFile()
+            new TestPackageBuilder(null, null, null, null, null, null)
+
+        expect:
+            TestPackageBuilder.testCasePath == null
+            TestPackageBuilder.testConfig == null
+            TestPackageBuilder.executionConfig == null
+            TestPackageBuilder.context == null
+    }
+
+    def 'Test Default Values'() {
+        given:
+            final StepContext context = Mock()
+            final String file = 'testFile'
             final TestConfig testConfig = new TestConfig()
             final ExecutionConfig executionConfig = new ExecutionConfig()
-            final PackageConfig packageConfig = new PackageConfig()
+            final PackageConfig packageConfig = new PackageConfig(null)
             final AnalysisConfig analysisConfig = new AnalysisConfig()
-            final AdditionalSettings additionalSettings = new AdditionalSettings()
-                    .forceConfigurationReload(testConfig.forceConfigurationReload)
-                    .packageParameters(packageConfig.packageParameters as List<LabeledValue>)
-                    .analysisName(analysisConfig.analysisName)
-                    .mapping(analysisConfig.mapping)
-                    .recordings(analysisConfig.recordings as List<Recording>)
-            TestPackageBuilder testPackageBuilder = new TestPackageBuilder(testFile.getAbsolutePath(), testConfig,
+            TestPackageBuilder builder = new TestPackageBuilder(file, testConfig,
                     executionConfig, context, packageConfig, analysisConfig)
 
         when:
-            ExecutionOrder executionOrder = testPackageBuilder.getExecutionOrder()
+            ExecutionOrder order = builder.getExecutionOrder()
 
         then:
-            executionOrder.getAdditionalSettings().equals(additionalSettings)
-            // executionOrder.getAdditionalSettings().getMapping().equals(HERE DEFINED EX. ORDER...)
-            executionOrder.getTbcPath() == ''
-            executionOrder.getTcfPath() == ''
-            executionOrder.getTestCasePath() == testFile.getAbsolutePath()
-            executionOrder.getConstants() == (List<LabeledValue>) testConfig.constants
-            executionOrder.getExecutionId() == ""
+            assertBuilder()
+            assertExecutionOrder(order)
     }
 
-    def 'Test Nonempty Execution Order'() {
-        given:
-            final File testFile = folder.newFile("test.pkg")
-            final File configFolder = folder.newFolder("Configurations")
-            final File testTbc = File.createTempFile("test", ".tbc", configFolder)
-            final File testTcf = File.createTempFile("test2", ".tcf", configFolder)
-            final List<Constant> testConstants = new ArrayList<>()
-            testConstants.add(new Constant("testLabel","testValue"))
-            final TestConfig testConfig = new TestConfig()
-            testConfig.setTbcPath(testTbc.getAbsolutePath())
-            testConfig.setTcfPath(testTcf.getAbsolutePath())
-            testConfig.setConstants(testConstants)
-            final ExecutionConfig executionConfig = new ExecutionConfig()
-            final List<PackageParameter> testParameters = new ArrayList<>()
-            testParameters.add(new PackageParameter("testPackageLabel","testPackageValue"))
-            final PackageConfig packageConfig = new PackageConfig(testParameters)
-            final AnalysisConfig analysisConfig = new AnalysisConfig()
-            analysisConfig.setAnalysisName("testName")
-            analysisConfig.setMapping("testMapping")
-            List<de.tracetronic.jenkins.plugins.ecutestexecution.model.Recording> testRecording = new ArrayList<>()
-            testRecording.add(new de.tracetronic.jenkins.plugins.ecutestexecution.model.Recording("testPath"))
-            analysisConfig.setRecordings(testRecording)
-            final AdditionalSettings additionalSettings = new AdditionalSettings()
-                    .forceConfigurationReload(testConfig.forceConfigurationReload)
-                    .packageParameters(packageConfig.packageParameters as List<LabeledValue>)
-                    .analysisName(analysisConfig.analysisName)
-                    .mapping(analysisConfig.mapping)
-                    .recordings(analysisConfig.recordings as List<Recording>)
-            TestPackageBuilder testPackageBuilder = new TestPackageBuilder(testFile.getAbsolutePath(), testConfig,
-                    executionConfig, context, packageConfig, analysisConfig)
-
-        when:
-            ExecutionOrder executionOrder = testPackageBuilder.getExecutionOrder()
-
-        then:
-            executionOrder.getAdditionalSettings().equals(additionalSettings)
-            executionOrder.getTbcPath() == testTbc.getAbsolutePath()
-            executionOrder.getTcfPath() == testTcf.getAbsolutePath()
-            executionOrder.getTestCasePath() == testFile.getAbsolutePath()
-            executionOrder.getConstants() == testConstants
-            executionOrder.getExecutionId() == ""
-            executionOrder.getAdditionalSettings().getMapping() == "testMapping"
-            executionOrder.getAdditionalSettings().getAnalysisName() == "testName"
-            executionOrder.getAdditionalSettings().getPackageParameters().size() == 1
-            executionOrder.getAdditionalSettings().getPackageParameters()[0].getLabel() == "testPackageLabel"
-            executionOrder.getAdditionalSettings().getPackageParameters()[0].getValue() == "testPackageValue"
+    def assertBuilder()  {
+        TestPackageBuilder.testCasePath != null
+        TestPackageBuilder.testConfig != null
+        TestPackageBuilder.executionConfig != null
+        TestPackageBuilder.context != null
     }
 
+    def assertExecutionOrder(ExecutionOrder order) {
+        order.additionalSettings != null
+        order.getTestCasePath() == 'testFile'
+        order.getTbcPath() == ''
+        order.getTcfPath() == ''
+        order.getConstants() != null
+        order.getExecutionId() == ''
+    }
 }
--- src/test/groovy/de/tracetronic/jenkins/plugins/ecutestexecution/builder/TestProjectBuilderIT.groovy
+++
@@ -1,7 +0,0 @@
-package de.tracetronic.jenkins.plugins.ecutestexecution.builder
-
-import de.tracetronic.jenkins.plugins.ecutestexecution.IntegrationTestBase
-import spock.lang.Specification
-
-class TestProjectBuilderIT extends IntegrationTestBase {
-}
